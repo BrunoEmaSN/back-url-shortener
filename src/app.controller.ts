03854dc621d0abeecc575c3707b191eeb213112a
BrunoESN <bsanchez@quorumit.com>
2024-03-18 18:08:53 -0300

    modified app flow: - add end points for create and get tiny urls - add services and database module in app module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, Post, Redirect, Res } from '@nestjs/common';
+import { TinyUrlsService } from './tiny-url/tiny-url.service';
+import { CreateTinyUrlDto } from './tiny-url/dtos/create-tiny-url.dto';
+import { TinyUrl } from './tiny-url/interfaces/tiny-url.interface';
+import { FindTinyUrlsDto } from './tiny-url/dtos/find-tiny-urls.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly tinyUrlsService: TinyUrlsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  async findAllByEmail(@Body() findTinyUrlsDto: FindTinyUrlsDto): Promise<TinyUrl[]> {
+    try {
+      return this.tinyUrlsService.findAllByEmail(findTinyUrlsDto.email);
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'No short url was found associated with the email',
+      }, HttpStatus.NOT_FOUND, {
+        cause: error
+      });
+    }
+  }
+
+  @Post('/create')
+  async create(@Body() createTinyUrlDto: CreateTinyUrlDto) {
+    try {
+      const result = await this.tinyUrlsService.create(createTinyUrlDto);
+      const shortUrl = `${process.env.DOMAIN}/${result.shortUrl}`;
+  
+      return shortUrl;
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'Could not create a new account',
+      }, HttpStatus.FORBIDDEN, {
+        cause: error
+      });
+    }
+  }
+
+  @Get('/:shortUrl')
+  async findByShortUrl(@Param('shortUrl') shortUrl: string, @Res() res) {
+    try {
+      const originalUrl = await this.tinyUrlsService.findOriginalUrlByShortUrl(shortUrl);
+  
+      return res.redirect(originalUrl)
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Short url not found',
+      }, HttpStatus.NOT_FOUND, {
+        cause: error
+      });
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TinyUrlsService } from './tiny-url/tiny-url.service';
+import { tinyUrlProviders } from './tiny-url/providers/tiny-url.providers';
+import { DatabaseModule } from './database/db.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    DatabaseModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [TinyUrlsService, ...tinyUrlProviders],
 })
 export class AppModule {}
