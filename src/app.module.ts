d8f02b355230ea9c3e03a39f0fb4064ac1d97f65
BrunoESN <bsanchez@quorumit.com>
2024-03-18 20:55:33 -0300

    add caching with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,23 @@ import { ConfigModule } from '@nestjs/config';
 import { TinyUrlsService } from './tiny-url/tiny-url.service';
 import { tinyUrlProviders } from './tiny-url/providers/tiny-url.providers';
 import { DatabaseModule } from './database/db.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT),
+          }
+        })
+      })
+    }),
     UsersModule,
     DatabaseModule
   ],
--- src/tiny-url/tiny-url.service.ts
+++ src/tiny-url/tiny-url.service.ts
@@ -1,7 +1,9 @@
 import { Model } from 'mongoose';
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { TinyUrl } from './interfaces/tiny-url.interface';
 import { CreateTinyUrlDto } from './dtos/create-tiny-url.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class TinyUrlsService {
@@ -10,6 +12,7 @@ export class TinyUrlsService {
   constructor(
     @Inject('TINY_URL_MODEL')
     private tinyUrlModel: Model<TinyUrl>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
   ) {}
 
   async create(CreateTinyUrlDto: CreateTinyUrlDto): Promise<TinyUrl> {
@@ -48,7 +51,25 @@ export class TinyUrlsService {
   }
 
   async findOriginalUrlByShortUrl(shortUrl: string): Promise<string> {
-    const shortUrls = await this.tinyUrlModel.findOne({ "shortUrl": shortUrl }).exec();
-    return shortUrls.originalUrl;
+    try {
+      const key = 'short-urls';
+      const shortUrlsCached: TinyUrl[] = await this.cacheManager.get(key) || [];
+      const shortUrlCached = shortUrlsCached.find(tinyUrl => tinyUrl.shortUrl === shortUrl);
+
+      if(shortUrlCached) {
+        return shortUrlCached.originalUrl;
+      }
+      
+      const shortUrls = await this.tinyUrlModel.findOne({ "shortUrl": shortUrl }).exec();
+      this.cacheManager.set(key, [...shortUrlsCached, shortUrls], 1000 * 10); 
+      return shortUrls.originalUrl;
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Short url not found',
+      }, HttpStatus.NOT_FOUND, {
+        cause: error
+      });
+    }
   }
 }
